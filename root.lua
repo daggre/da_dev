05533a6db411ca8f1b3c15ea78830731940d3c87
daggre_actual <dgract@gmail.com>
2024-04-30 00:13:17 -0700

    Update menu and env options

--- root.lua
+++ root.lua
@@ -1,7 +1,7 @@
 da.Dev.Menu.RegisterOption("root", "noclip", "z", function() da.Dev.NoClip() end)
-da.Dev.Menu.RegisterOption("root", "revive", "r", function() TriggerEvent('TMC:Command:Revive') end)
+da.Dev.Menu.RegisterOption("root", "revive", "r", function() TriggerEvent('TMC:Command:Revive') end, function() return LocalPlayer.state.metadata.isdead end)
 
-da.Dev.Menu.RegisterOption("root", "stats", "s", function()
+da.Dev.Menu.RegisterOption("root", "max ply stat", "s", function()
     local playerPedId = PlayerPedId()
     for i = 0, 1 do
         Citizen.InvokeNative(0xC6258F41D86676E0, playerPedId, i, 100) -- SetAttributeCoreValue
