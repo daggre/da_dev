65b0859a5f03a801e2a41851f18c31fcf51f772d
daggre_actual <dgract@gmail.com>
2024-09-02 23:09:05 -0700

    Add globalstate client command

--- cmd/client.lua
+++ cmd/client.lua
@@ -1,3 +1,16 @@
--- da = exports.da_lib:importLib()
--- da.Log.Register(function(msg) print(msg) end)
---
+if da.Util.IsDev then
+    RegisterCommand("globalstate", function(source, args, rawCommand)
+        if args[1] == "get" then
+            local key = args[2]
+            da.Log.Info(GlobalState[key])
+        elseif args[1] == "set" then
+            local key = args[2]
+            local value = args[3]
+            local pValue = GlobalState[key]
+            if key and value then
+                GlobalState[key] = value
+                da.Log.Info(("%s -> %s"):format(pValue, GlobalState[key]))
+            end
+        end
+    end, false)
+end
