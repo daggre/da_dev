39b28057447d41a5e3003b8c288424eed72efb21
daggre_actual <dgract@gmail.com>
2024-09-02 23:08:43 -0700

    Update weather dev commands

--- env.lua
+++ env.lua
@@ -1,58 +1,6 @@
-local freezeTime = true
-local freezeWeather = true
-local freezeWind = true
-
-local currentHour = 12
-local currentMinute = 0
-
 da.Dev.Menu.RegisterMenu("root", "environ", "w")
 da.Dev.Menu.RegisterMenu("environ", "time", "t")
 da.Dev.Menu.RegisterMenu("environ", "weather", "w")
-da.Dev.Menu.RegisterMenu("environ", "wind", "d")
-
--- da.Dev.Menu.RegisterOption("wind", "frz wind", "z", function()
---     freezeWind = true
---     TMC.Functions.TriggerServerEvent("weather:server:set", { type = "wind", isFrozen = freezeWind, })
--- end, function() return not freezeWind end)
-
--- da.Dev.Menu.RegisterOption("wind", "unfrz wind", "z", function()
---     freezeWind = false
---     TMC.Functions.TriggerServerEvent("weather:server:set", { type = "wind", isFrozen = freezeWind, })
--- end, function() return freezeWind end)
-
--- da.Dev.Menu.RegisterOption("weather", "unfrz wthr", "z", function()
---     freezeWeather = false
---     TMC.Functions.TriggerServerEvent("weather:server:set", { type = "weather", isFrozen = freezeWeather, })
--- end, function() return freezeWeather end)
-
--- da.Dev.Menu.RegisterOption("weather", "frz wthr", "z", function()
---     freezeWeather = true
---     TMC.Functions.TriggerServerEvent("weather:server:set", { type = "weather", isFrozen = freezeWeather, })
--- end, function() return not freezeWeather end)
-
--- da.Dev.Menu.RegisterOption("time", "frz time", "z", function()
---     freezeTime = true
---     TMC.Functions.TriggerServerEvent("weather:server:set", {
---         type = "time",
---         isFrozen = freezeTime,
---         day = 1,
---         hour = currentHour or 12,
---         minute = currentMinute or 0,
---         second = 0,
---     })
--- end, function() return not freezeTime end)
-
--- da.Dev.Menu.RegisterOption("time", "unfrz time", "z", function()
---     freezeTime = false
---     TMC.Functions.TriggerServerEvent("weather:server:set", {
---         type = "time",
---         isFrozen = freezeTime,
---         day = 1,
---         hour = currentHour or 12,
---         minute = currentMinute or 0,
---         second = 0,
---     })
--- end, function() return freezeTime end)
 
 local timeOptions = {
     { name = "dawn", hour = 5, minute = 30 },
@@ -67,29 +15,41 @@ local timeOptions = {
 
 for i, option in ipairs(timeOptions) do
     da.Dev.Menu.RegisterOption("time", option.name, tostring(i), function()
+        -- Client Only
         local second = 0
+        local freezeTime = true
         Citizen.InvokeNative(0x669E223E64B1903C, option.hour, option.minute, second, 0, freezeTime) -- NetworkClockTimeOverride
     end)
 end
 
 local weatherTypes = {
-    blizzard = "blizzard",
-    cloudy = "clouds",
-    drizzle = "drizzle",
-    fog = "fog",
-    hail = "hail",
-    sunny = "highpressure",
-    windyrain = "hurricane",
-    mist = "misty",
+    blizzard = "b",
+    cloudy = "c",
+    drizzle = "d",
+    fog = "f",
+    groundblizzard = "g",
+    hail = "h",
+    highpressure = "p",
+    hurricane = "u",
+    misty = "m",
+    overcast = "o",
+    overcastdark = "k",
+    rain = "r",
+    sandstorm = "s",
+    showers = "w",
+    sleet = "l",
+    snow = "n",
+    snowlight = "i",
+    sunny = "y",
+    thunder = "t",
+    thunderstorm = "z",
+    whiteout = "x",
 }
 
--- for title, weatherName in pairs(weatherTypes) do
---     da.Dev.Menu.RegisterOption("weather", title, title:sub(1, 1), function()
---         freezeWeather = true
---         TMC.Functions.TriggerServerEvent("weather:server:set", {
---             type = "weather",
---             isFrozen = freezeWeather,
---             weather = weatherName,
---         })
---     end)
--- end
+for weatherName, key in pairs(weatherTypes) do
+    da.Dev.Menu.RegisterOption("weather", weatherName, key, function()
+        -- Client Only
+        local transitionTime = 10 -- Seconds
+        Citizen.InvokeNative(0x59174F1AFE095B5A, GetHashKey(weatherName), true, false, true, transitionTime, false) -- SetWeatherType
+    end)
+end
