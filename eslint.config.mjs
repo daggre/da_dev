ebf3f64282b903572e84fc3b5f8c73a1159c3cd9
daggre_actual <dgract@gmail.com>
2025-02-25 11:47:10 -0800

    Configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,13 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    {
+        languageOptions: { globals: globals.browser },
+        ignores: [
+            "**/ui/web/assets/**", // Ignore all matching paths recursively
+            "ui/web/assets/**",   // Standard ignore
+            "./ui/web/assets/**", // Explicit relative ignore
+        ],
+    },
+    pluginJs.configs.recommended,
 ];
