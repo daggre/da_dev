add622bd9305dd67f854234a72c39d39d95ca1dc
daggre_actual <dgract@gmail.com>
2025-12-04 13:01:24 -0800

    Add log for copy to clipboard in the UI

--- ui/web/src/clipboard.js
+++ ui/web/src/clipboard.js
@@ -3,6 +3,7 @@ export function clipboardCopy(val) {
     textArea.value = val;
     document.body.appendChild(textArea);
     textArea.select();
+    console.log("Copy:", val);
     document.execCommand('copy');
     document.body.removeChild(textArea);
 }
